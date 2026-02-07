2465a02335d847399815d8937d16a9aa8a278d7f
NikolStef <nstefas125@gmail.com>
2026-02-07 15:34:47 -0500

    added unit test

--- TCPsocketsSPSCparser_sender.cpp
+++ TCPsocketsSPSCparser_sender.cpp
@@ -1,4 +1,4 @@
-// To do: BodyLength+CheckSum compliant FIX msg generator
+// to do: Separate unit tests
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <thread>
@@ -6,8 +6,9 @@
 #include <string>
 #include "SPSCqueue.h"
 #include <vector>
+#include <chrono>
 
-// Link against the Windows Sockets library - would modify CMakeLists for proper production
+// Link against the Windows Sockets library - modify CMakeLists in production
 #pragma comment(lib, "Ws2_32.lib")
 
 SQueue<FixMessage, 1024> sendQueue;
@@ -58,27 +59,129 @@ int main() {
 
 	// FIX msgs examples
 	std::vector<std::string> fix;
+
+	// valid msg - short
 	fix.emplace_back(
-				"8=FIX.4.4\x01"
-				"9=69\x01"
-				"35=A\x01"
-				"49=CLIENT1\x01"
-				"56=SERVER1\x01"
-				"34=1\x01"
-				"52=20260205-14:32:10.000\x01"
-				"98=0\x01"
-				"108=30\x01"
-				"10=233\x01");
+		"8=FIX.4.2\x01"
+		"9=5\x01"
+		"35=0\x01"
+		"10=161\x01");
+
+	// valid msg - long
+	fix.emplace_back(
+		"8=FIX.4.4\x01"
+		"9=69\x01"
+		"35=A\x01"
+		"49=CLIENT1\x01"
+		"56=SERVER1\x01"
+		"34=1\x01"
+		"52=20260205-14:32:10.000\x01"
+		"98=0\x01"
+		"108=30\x01"
+		"10=233\x01");
+
+	// 1. Standard heartbeat
+	fix.emplace_back(
+		"8=FIX.4.4\x01"
+		"9=5\x01"
+		"35=0\x01"
+		"10=163\x01");
+
+	// 3. Garbage before message
+	fix.emplace_back(
+		"garbage" "\x01"
+		"8=FIX.4.4" "\x01"
+		"9=5" "\x01"
+		"35=0" "\x01"
+		"10=163" "\x01");
+
+	// Corrupted prefix and recovery: Parser should recover and parse message after "8=FIX"
+	fix.emplace_back(
+		"junk8=FIX.4.4" "\x01"
+		"9=5" "\x01"
+		"35=0" "\x01"
+		"10=163" "\x01"
+	);
+
+	// Message with nested SOH inside value (illegal): parser should reject this and move on
+	fix.emplace_back(
+		"8=FIX.4.4" "\x01"
+		"9=9" "\x01"
+		"35=D" "\x01"
+		"55=ABC" "\x01" "DEF" "\x01"
+		"10=123" "\x01"
+	);
+
+	// Partial/incomplete message: parser should wait for the next msg and process them together
+	fix.emplace_back(
+		"8=FIX.4.4" "\x01"
+		"9=5" "\x01"
+		"35=0" "\x01"
+	);
+	fix.emplace_back(
+		"10=163" "\x01"
+	);
+
+
+	// Two messages in one
+	fix.emplace_back(
+		"8=FIX.4.4" "\x01"
+		"9=5" "\x01"
+		"35=0" "\x01"
+		"10=163" "\x01"
+		"8=FIX.4.2\x01"
+		"9=5\x01"
+		"35=0\x01"
+		"10=161\x01"
+	);
+
+	// empty msg: parser shouldn't be affected
+	fix.emplace_back(
+
+	);
+
+
+	// BodyLength mismatch: paraser should reject
+	fix.emplace_back(
+		"8=FIX.4.4" "\x01"
+		"9=4" "\x01"
+		"35=0" "\x01"
+		"10=163" "\x01"
+	);
+
+	// 7. Wrong checksum: parse should outpout "bad checksum" and continue
+	fix.emplace_back(
+		"8=FIX.4.4" "\x01"
+		"9=5" "\x01"
+		"35=0" "\x01"
+		"10=165" "\x01"
+	);
+
+	// 9. MsgType (35=) must come immediately after BodyLength: parser should reject as malformed
+	fix.emplace_back(
+		"8=FIX.4.4\x01"
+		"9=69\x01"
+		"49=CLIENT1\x01"
+		"35=A\x01"
+		"56=SERVER1\x01"
+		"34=1\x01"
+		"52=20260205-14:32:10.000\x01"
+		"98=0\x01"
+		"108=30\x01"
+		"10=233\x01");
+
 
 	std::thread producer([&] {
-		for (int i = 0; i < 3; ++i) {
-			const char* msg_out = fix[0].c_str();
+		for (int i = 0; i < 20; ++i) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(11));
+			std::string fix_msg = fix[i%fix.size()];
+			const char* msg_out = fix_msg.c_str();
 
 			FixMessage msg{};
 
 			// Formats FIX msg into msg.data
-			msg.len = fix[0].size();
-			memcpy(msg.data, fix[0].c_str(), msg.len);
+			msg.len = fix_msg.size();
+			memcpy(msg.data, fix_msg.c_str(), msg.len);
 
 			// non blocking enqueue attempt
 			while (!sendQueue.enqueue(msg)) _mm_pause();
